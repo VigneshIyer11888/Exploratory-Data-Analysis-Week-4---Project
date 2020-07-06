b4787747ab480098a54ef43336294e138d6fe36e
VigneshIyer11888 <vigneshiyer11888@gmail.com>
2020-07-06 15:28:19 +0530

    Modified files

--- plot2.R
+++ plot2.R
@@ -7,11 +7,11 @@ SCC <- readRDS("./Data for Peer Assessment/Source_Classification_Code.rds")
 setwd("C:/Users/DELL/OneDrive/Desktop/Coursera Folder/Exploratory Data Analysis Week 4 - Project")
 
 # Subset NEI data by Baltimore's fip.
-baltimoreNEI <- NEI[NEI$fips=="24510",]
+subsetBaltimoreNEI <- NEI[NEI$fips=="24510",]
 
 # Aggregate Baltimore emissions
 aggBaltimoreEmissions <- aggregate(Emissions ~ year,
-                                   baltimoreNEI, sum)
+                                   subsetBaltimoreNEI, sum)
 
 # plotting the graph
 png("plot2.png",width=480,height=480,units="px",bg="transparent")
