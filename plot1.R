5658dce81376400c69f469853654e66a5a9c499f
VigneshIyer11888 <vigneshiyer11888@gmail.com>
2020-07-06 14:44:20 +0530

    Modified files

--- plot1.R
+++ plot1.R
@@ -11,3 +11,21 @@ unzip(zipfile = "./Data for Peer Assessment.zip",
 NEI <- readRDS("./Data for Peer Assessment/summarySCC_PM25.rds")
 SCC <- readRDS("./Data for Peer Assessment/Source_Classification_Code.rds")
 
+# We first need to find the aggregate total PM2.5 emission from all sources for 
+# each of the years 1999, 2002, 2005, and 2008.
+aggTotalEmissions <- aggregate(Emissions ~ year,NEI, sum)
+
+# Have total emissions from PM2.5 decreased in the United States 
+# from 1999 to 2008? Using the base plotting system, make a plot 
+# showing the total PM2.5 emission from all sources for each of 
+# the years 1999, 2002, 2005, and 2008.
+
+png('plot1.png')
+
+barplot(height=aggTotalEmissions$Emissions, 
+        names.arg=aggTotalEmissions$year, 
+        xlab="years", 
+        ylab=expression('total PM'[2.5]*' emission'),
+        main=expression('Total PM'[2.5]*' emissions at various years'))
+
+dev.off()
