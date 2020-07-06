e2ec848045618a539d7e3c348f51aa07e5dc98f7
VigneshIyer11888 <vigneshiyer11888@gmail.com>
2020-07-06 13:42:01 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,3 +1,4 @@
+# Downloading the compressed file and extracting it to the source location
 
 dataFile <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
 
@@ -6,6 +7,7 @@ download.file(dataFile, destfile = "./Data for Peer Assessment.zip")
 unzip(zipfile = "./Data for Peer Assessment.zip",
       exdir = "./Data for Peer Assessment")
 
+# Loading the file data
 NEI <- readRDS("./Data for Peer Assessment/summarySCC_PM25.rds")
 SCC <- readRDS("./Data for Peer Assessment/Source_Classification_Code.rds")
 
